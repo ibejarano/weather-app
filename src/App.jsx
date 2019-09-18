9178dde4db2ff3c68068c976ebe7206c06777f2f
Ignacio Bejarano <ignbejarano@gmail.com>
2019-09-18 18:33:19 -0300

    implemented image in bottom section o

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './sass/app.scss'
 
 
 import TopSection from './components/top/index';
+import BottomSection from './components/bottom/index';
 
 
 class App extends React.Component {
@@ -12,7 +13,7 @@ class App extends React.Component {
     return <div className="app-container">
       <div className="main-container">
         <div className="top-section"> <TopSection /> </div>
-        <div className="bottom-section">BOTTOM</div>
+        <div className="bottom-section"> <BottomSection /> </div>
       </div>
 
     </div>
--- src/components/bottom/index.jsx
+++ src/components/bottom/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './style.scss';
+
 export default class BottomSection extends React.Component {
     constructor(props){
         super(props);
@@ -7,6 +9,6 @@ export default class BottomSection extends React.Component {
     }
 
     render () {
-        return <div> BOTTOM SECTION </div>
+        return <div className="bottom-container"> BOTTOM SECTION !!! </div>
     }
 }
