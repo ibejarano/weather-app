e4c6bb1a482ac020bf1d6c1ad028782495b0f29c
Ignacio Bejarano <ignbejarano@gmail.com>
2019-09-20 17:13:56 -0300

    added forcast component

---
+++ src/components/bottom/forcastday.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Forcastday extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return <div className="forcastday-container">
+            <div className="image">
+                <div className="text"><img src="" alt="imagehere"/>a</div>
+            </div>
+        </div>
+    }
+
+}
