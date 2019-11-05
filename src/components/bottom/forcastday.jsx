acb8bcd022bc9daa833ba7e3b063656143d3394b
Ignacio Bejarano <ignbejarano@gmail.com>
2019-11-05 20:38:15 -0300

    wrapped forecast day component

--- src/components/bottom/forcastday.jsx
+++ src/components/bottom/forcastday.jsx
@@ -1,17 +1,45 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography'
 
-export default class Forcastday extends React.Component {
-
-    render() {
-
-        const { main,  weather } = this.props.weather
-
-        const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`
-
-        return <div className="forcastday-one-container">
-                <img src={iconUrl} alt="forcast img"/>
-                <div className="text">{(main.temp - 273).toFixed(1)}</div>
-        </div>
+const useStyles = makeStyles(theme => ({
+    root: {
+        padding: theme.spacing(1, 1),
+        width: 80,
+        height: 80,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'violet',
+        flexDirection: 'column'
+    },
+    image: {
+    width: '80%'
+    },
+    text: {
+        color: 'white'
     }
+  }));
 
+export default function Forcastday(props) {
+    const classes = useStyles();
+
+
+    const { main,  weather } = props.weather
+
+    const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`
+
+    return ( 
+        <React.Fragment>
+
+        <Paper className={classes.root} elevation="3" >
+            <img className={classes.image} src={iconUrl} alt="forcast img"/>
+            <Typography className={classes.text} variant="h6" component="h2">
+                {`${(main.temp - 273).toFixed(1)} °C`}
+            </Typography>
+        </Paper>
+        </React.Fragment>
+
+    );
 }
